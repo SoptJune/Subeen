53d4ac0fbac8a5553c1c244fed86aa65f6db2894
kimsubeen <papajj06@naver.com>
2022-12-01 01:29:41 +0900

    프로그래머스/입국심사 (진짜코드..)

--- Programmers/입국심사.py
+++ Programmers/입국심사.py
@@ -6,3 +6,26 @@ Created on Wed Nov 30 22:48:43 2022
 @author: kimsubin
 """
 
+def solution(n, times):
+    answer = 0
+   
+    left = min(times)
+    right = max(times)*n
+    
+    while left <= right:
+        mid = (left+ right) // 2
+        check = 0
+        
+        for time in times:
+            check += mid // time
+            if check >= n:
+                break
+        
+        if check >= n:
+            answer = mid
+            right = mid - 1
+      
+        elif check < n:
+            left = mid + 1
+            
+    return answer

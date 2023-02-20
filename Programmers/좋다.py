a861950ec73de445ce1e8dc1426e06665b26fa8c
kimsubeen <papajj06@naver.com>
2023-02-20 23:16:56 +0900

    주석 추가

--- Programmers/좋다.py
+++ Programmers/좋다.py
@@ -9,17 +9,17 @@ for i in range(N):
     left = 0
     right =N-1
     while left < right:
-        if number[left]+number[right] == number[i]:
-            if left !=i and right !=i:
+        if number[left]+number[right] == number[i]: #1
+            if left !=i and right !=i: #2개 모두 각 다른 자리 수 일때
                 count+=1
                 break
-            elif left ==i:
+            elif left ==i: #left만 같을때
                 left+=1
-            else:
+            else: #right만 같을때
                 right-=1
-        elif number[left]+number[right]>number[i]:
+        elif number[left]+number[right]>number[i]:#2
             right-=1
-        else:
+        else: #3
             left+=1
 
 print(count)
